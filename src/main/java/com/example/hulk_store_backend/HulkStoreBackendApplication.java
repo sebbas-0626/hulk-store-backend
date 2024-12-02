5174234e4f92dd44ed88541fff4119ae4f7b9466
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-12-02 16:20:13 -0500

    :tada: start project

---
+++ src/main/java/com/example/hulk_store_backend/HulkStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hulk_store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulkStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HulkStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hulk_store_backend/HulkStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hulk_store_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulkStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
