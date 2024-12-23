0123a0d109501d328b367d087cc0e58363cc2518
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-12-23 00:03:45 -0500

    feat: :sparkles: se creo la tabla Product y le hizo la relacion con la tabla category

---
+++ src/main/java/com/example/hulk_store_backend/model/Product.java
@@ -0,0 +1,28 @@
+package com.example.hulk_store_backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @Column(nullable = false)
+    private Double price;
+    @Column(nullable = false)
+    private Integer stock;
+    @Column(name = "image_url")
+    private String imageUrl;
+    // Relación Muchos a Uno con Category
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+    private Boolean status;
+}
